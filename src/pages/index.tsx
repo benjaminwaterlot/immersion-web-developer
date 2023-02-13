17fbc2ef514b70c0a95184a4e4a18da13f753c64
Benjamin Waterlot <waterlotbenjamin@gmail.com>
2023-02-13 15:20:59 +0100

    first part with sophie

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,28 @@ import "./index.css";
 const IndexPage: React.FC<PageProps<Queries.IndexPageQuery>> = ({ data }) => {
   console.log(data.datoCmsSite?.name);
 
+  const options: any = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const LocaleDate: any = data.datoCmsArticle?.date;
+  const LocaleDateString = new Date(LocaleDate).toLocaleDateString(
+    "fr",
+    options
+  );
+
   return (
     <main>
-      <h1 className="title">Hello world</h1>
+      <div className="header">
+        <h1 className="title">{data.datoCmsSite?.name}</h1>
+      </div>
+      <div className="mainTitle">
+        <h2 className="PostTitle">{data.datoCmsArticle?.title}</h2>
+        <span className="publishedDate">publié le {LocaleDateString}</span>
+      </div>
+      <div className="content"></div>
     </main>
   );
 };
@@ -21,6 +40,14 @@ export const pageQuery = graphql`
   query IndexPage {
     datoCmsSite {
       name
+    }
+
+    datoCmsArticle(slug: { eq: "creation-previsionnel-tresorerie" }) {
+      title
+      date
+      body {
+        ...RichText
+      }
     }
 
     # an article can be fetched here using the 'datoCmsArticle' field.
